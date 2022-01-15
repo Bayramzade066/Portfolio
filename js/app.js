9f980631eb9ca40c6d5a57423eed1437114e14a1
Bayramzade066 <bayramzade.313@gmail.com>
2022-01-15 16:44:11 +0400

    update

--- js/app.js
+++ js/app.js
@@ -1,3 +1,37 @@
+// cursor
+// const root = document.querySelector('html')
+
+// // Real cursor element
+// const cursor = document.createElement('div')
+// cursor.classList.add('cursor')
+// root.appendChild(cursor)
+
+// // Following extra cursor element
+// const follower = document.createElement('div')
+// follower.classList.add('cursor', 'cursor__follower')
+// root.appendChild(follower)
+
+
+// document.addEventListener('mousemove', (e) => {
+//     setPosition(follower, e)
+//     setPosition(cursor, e)
+    
+// })
+
+// // root.addEventListener('scroll',()=>{
+
+// // })
+
+// function setPosition(element, e) {
+//   element.style.transform = `translate3d(${e.clientX}px, ${e.clientY}px, 0)`
+// }
+
+
+
+
+
+
+// section change
 let arr = [
  document.querySelector(".home"),
  document.querySelector(".about"),
@@ -8,7 +42,7 @@ document.querySelector(".blogs"),
 
  let nav__menu = document.querySelectorAll(".menu-li")
  
-// section change
+
 for(let i =0;i<nav__menu.length;i++){
         nav__menu[i].addEventListener("click",()=>{
             arr[i].style.opacity="1"
@@ -31,6 +65,7 @@ for(let i =0;i<nav__menu.length;i++){
 }
 
 
+
 // preloader
 let preloader = document.querySelector(".preloader")
 
@@ -45,6 +80,27 @@ let preloader = document.querySelector(".preloader")
         preloader.style.display="none"
     }, 3000);
     
+
+
 // music
-
-
+var state=0;
+$(document).ready(function(){
+    $(".power").click(function(){
+          $(".record").toggleClass('on');
+          $(".stick").toggleClass('play');
+          if(state==0){
+              setTimeout(function(){
+               $(".mysong")[0].play();
+              },1000);
+              state=1;
+          }
+          else{
+              $(".mysong")[0].pause();
+              state=0;
+          }
+          $(".slider").change(function(){
+              $(".mysong")[0].volume=this.value;
+          });
+      });
+      
+});

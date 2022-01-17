63e848b31c72dce9f4443f2ac5a0e94fa664f28f
Bayramzade066 <bayramzade.313@gmail.com>
2022-01-17 11:11:08 +0400

    newUpdate

--- js/app.js
+++ js/app.js
@@ -100,3 +100,55 @@ $(document).ready(function(){
       });
       
 });
+
+
+
+// writing animation
+
+var txt2write = new Array(
+    "Writing code is like poetry — it’s a form of art.",
+    "Staying motivated should be the top priority of any",
+    "aspiring developer in order to achieve the highest",
+    "possible quality of code."
+    );
+
+// Variables
+var speed = 40; // You can set the speed here. + is slower
+var index = 0;
+text_pos = 0;
+var str_length = txt2write[0].length;
+var contents, row;
+
+// Function
+function type_text() {
+    // Init the content with blank
+    contents = '';
+    row = Math.max(0, index - 9);
+    while (row < index) {
+        // Each sentence will end with a <br />
+        contents += txt2write[row++] + '\r<br />';
+    }
+
+    // Write the text
+    $( "div.writeit" ).html( contents + txt2write[index].substring(0, text_pos) + "<span class='after'>_</span>" );
+
+    if (text_pos++ == str_length) {
+        text_pos = 0;
+        index++;
+        if (index != txt2write.length) {
+            str_length = txt2write[index].length;
+            setTimeout("type_text()", 800);
+        }
+    } else {
+        setTimeout("type_text()", speed);
+    }
+
+}
+
+
+
+let about = document.querySelector("#about")
+
+about.addEventListener('click',()=>{
+    type_text();
+})
